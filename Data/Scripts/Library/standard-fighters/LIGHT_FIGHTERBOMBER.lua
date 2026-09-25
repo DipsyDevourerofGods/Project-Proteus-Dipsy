d527f01a9419affadb69ddda518a693746ccf828
DipsyDevourerofGods <137007881+DipsyDevourerofGods@users.noreply.github.com>
2026-09-25 15:43:42 -0500

    Screed Roster Tweaks + New Slavery Buildings
    - YZ-775 Replaces Carrack
    - Gladiator-II Replaces Gladiator-I
    - ATL Interceptor replaces V-Wing in Fighter slot
    - V-Wing replaces Missile TIE/ln in Light Fighter-Bomber slot
    - New Structures for Screed (Slave Market and Slave Barracks)
        * Text and Icons to come later

--- Data/Scripts/Library/standard-fighters/LIGHT_FIGHTERBOMBER.lua
+++ Data/Scripts/Library/standard-fighters/LIGHT_FIGHTERBOMBER.lua
@@ -96,7 +96,7 @@ return {
 			CENTRALITY = {"Z95ML_HEADHUNTER_SQUADRON", false},
 			ANAXES = {"TIE_GT_SQUADRON", false},
 			SECRET = {"TIE_INTERCEPTOR_ION_SQUADRON", false},
-			SCREED = {"MISSILE_TIE_FIGHTER_SQUADRON", false},
+			SCREED = {"NIMBUS_V_WING_SQUADRON", false},
 		}
 		
 		if simpletypes[owner] then
